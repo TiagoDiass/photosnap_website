332d0f3e8d380590d7f742770ef015f9ead8b67c
Tiago Dias <tiago.costadiasss@gmail.com>
2021-04-28 12:27:22 -0300

    refactor: add comment on StoryCard component

--- components/StoryCard/StoryCard.js
+++ components/StoryCard/StoryCard.js
@@ -1,6 +1,16 @@
 import * as S from './StoryCard.elements';
 import { Arrow } from '..';
 
+/**
+ * componente StoryCard
+ * @param {object} props
+ * @param {object} props.story
+ * @param {string} props.story.imageName - nome da imagem do StoryCard
+ * @param {string} props.story.date - data do StoryCard
+ * @param {string} props.story.title - título do StoryCard
+ * @param {string} props.story.author - autor da story do StoryCard
+ * @returns
+ */
 export default function StoryCard({ story }) {
   return (
     <S.StoryContainer backgroundImageName={story.imageName}>
